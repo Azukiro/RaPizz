7d5eaf6ec353423a9cd5eb40d4a54b4d62471d44
Loïc FOURNIER <loic25112013@gmail.com>
2021-06-01 19:23:15 +0200

    Update create.sql

--- DB/create.sql
+++ DB/create.sql
@@ -1,14 +1,28 @@
-DROP TABLE IF EXISTS Ingredients;
+CREATE TABLE Pizzas(
+   id_pizza INT,
+   label VARCHAR(20) NOT NULL,
+   price DOUBLE NOT NULL,
+   PRIMARY KEY(id_pizza),
+   UNIQUE(label)
+);
+
+CREATE TABLE PizzaSizes(
+   id_size INT,
+   label VARCHAR(8) NOT NULL,
+   multiplicator DOUBLE NOT NULL,
+   PRIMARY KEY(id_size),
+   UNIQUE(label)
+);
+
 CREATE TABLE Ingredients(
-   id_ingredient INT AUTO_INCREMENT,
+   id_ingredient INT,
    label VARCHAR(20) NOT NULL,
    PRIMARY KEY(id_ingredient),
    UNIQUE(label)
 );
 
-DROP TABLE IF EXISTS DeliveryGuys;
 CREATE TABLE DeliveryGuys(
-   id_delivery_guy INT AUTO_INCREMENT,
+   id_delivery_guy INT,
    firstname VARCHAR(30) NOT NULL,
    lastname VARCHAR(30) NOT NULL,
    phone CHAR(10) NOT NULL,
@@ -16,24 +30,21 @@ CREATE TABLE DeliveryGuys(
    PRIMARY KEY(id_delivery_guy)
 );
 
-DROP TABLE IF EXISTS VehicleTypes;
 CREATE TABLE VehicleTypes(
-   id_vehicle_types INT AUTO_INCREMENT,
+   id_vehicle_types INT,
    label VARCHAR(10) NOT NULL,
    PRIMARY KEY(id_vehicle_types),
    UNIQUE(label)
 );
 
-DROP TABLE IF EXISTS City;
 CREATE TABLE City(
    zip CHAR(5),
    name VARCHAR(30),
    PRIMARY KEY(zip, name)
 );
 
-DROP TABLE IF EXISTS Vehicles;
 CREATE TABLE Vehicles(
-   id_vehicle INT AUTO_INCREMENT,
+   id_vehicle INT,
    licence_plate CHAR(9),
    label VARCHAR(20) NOT NULL,
    id_vehicle_types INT NOT NULL,
@@ -41,9 +52,8 @@ CREATE TABLE Vehicles(
    FOREIGN KEY(id_vehicle_types) REFERENCES VehicleTypes(id_vehicle_types)
 );
 
-DROP TABLE IF EXISTS Clients;
 CREATE TABLE Clients(
-   id_client INT AUTO_INCREMENT,
+   id_client INT,
    firstname VARCHAR(20) NOT NULL,
    lastname VARCHAR(20) NOT NULL,
    phone CHAR(10) NOT NULL,
@@ -54,9 +64,8 @@ CREATE TABLE Clients(
    FOREIGN KEY(zip, name) REFERENCES City(zip, name)
 );
 
-DROP TABLE IF EXISTS Account;
 CREATE TABLE Account(
-   id_login INT AUTO_INCREMENT,
+   id_login INT,
    mail VARCHAR(50) NOT NULL,
    password VARCHAR(50) NOT NULL,
    account_balance DOUBLE NOT NULL,
@@ -67,36 +76,8 @@ CREATE TABLE Account(
    FOREIGN KEY(id_client) REFERENCES Clients(id_client)
 );
 
-DROP TABLE IF EXISTS Composing;
-CREATE TABLE Composing(
-   id_pizza INT,
-   id_ingredient INT,
-   PRIMARY KEY(id_pizza, id_ingredient),
-   FOREIGN KEY(id_pizza) REFERENCES Pizzas(id_pizza),
-   FOREIGN KEY(id_ingredient) REFERENCES Ingredients(id_ingredient)
-);
-
-DROP TABLE IF EXISTS Pizzas;
-CREATE TABLE Pizzas(
-   id_pizza INT AUTO_INCREMENT,
-   label VARCHAR(20) NOT NULL,
-   price DOUBLE NOT NULL,
-   PRIMARY KEY(id_pizza),
-   UNIQUE(label)
-);
-
-DROP TABLE IF EXISTS PizzaSizes;
-CREATE TABLE PizzaSizes(
-   id_size INT AUTO_INCREMENT,
-   label VARCHAR(8) NOT NULL,
-   multiplicator DOUBLE NOT NULL,
-   PRIMARY KEY(id_size),
-   UNIQUE(label)
-);
-
-DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders(
-   id_order INT AUTO_INCREMENT,
+   id_order INT,
    order_timestamp DATETIME NOT NULL,
    delivry_timestamp DATETIME,
    id_size INT NOT NULL,
@@ -112,8 +93,10 @@ CREATE TABLE Orders(
    FOREIGN KEY(id_pizza) REFERENCES Pizzas(id_pizza)
 );
 
-
-
-
-
-
+CREATE TABLE Composing(
+   id_pizza INT,
+   id_ingredient INT,
+   PRIMARY KEY(id_pizza, id_ingredient),
+   FOREIGN KEY(id_pizza) REFERENCES Pizzas(id_pizza),
+   FOREIGN KEY(id_ingredient) REFERENCES Ingredients(id_ingredient)
+);
