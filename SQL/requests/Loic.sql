17631ed352d6e14524aada140f25a30f3fda4927
Loïc FOURNIER <loic25112013@gmail.com>
2021-06-02 09:56:58 +0200

    Update Loic.sql

--- SQL/requests/Loic.sql
+++ SQL/requests/Loic.sql
@@ -1,48 +1,77 @@
----Meilleur Client
+-- Meilleur Client
 SELECT
-    C.firstname,
-    C.lastname,
-    O.id_client,
-    COUNT(O.id_client) AS nbOrders
+    clie.firstname,
+    clie.lastname,
+    orde.id_client,
+    COUNT(orde.id_client) AS nbOrders
 FROM
-    orders O
-INNER JOIN clients C ON
-    O.id_client = C.id_client
+    orders orde
+INNER JOIN clients clie ON
+    orde.id_client = clie.id_client
 GROUP BY
-    O.id_client
+    orde.id_client
 ORDER BY
     nbOrders
 DESC
 LIMIT 1;
 
---Meilleur pizza
+-- Meilleur pizza
 SELECT
-    P.label,
-    O.id_pizza,
-    COUNT(O.id_pizza) AS nbOrders
+    pizz.label,
+     orde.id_pizza,
+    COUNT( orde.id_pizza) AS nbOrders
 FROM
-    orders O
-INNER JOIN pizzas P ON
-    O.id_pizza = P.id_pizza
+    orders  orde
+INNER JOIN pizzas pizz ON
+    orde.id_pizza = pizz.id_pizza
 GROUP BY
-    O.id_pizza
+    orde.id_pizza
 ORDER BY
     nbOrders
 DESC
 LIMIT 1;
 
---Pire pizza
+-- Pire pizza
 SELECT
-    P.label,
-    O.id_pizza,
-    COUNT(O.id_pizza) AS nbOrders
+    pizz.label,
+     orde.id_pizza,
+    COUNT( orde.id_pizza) AS nbOrders
 FROM
-    orders O
-INNER JOIN pizzas P ON
-    O.id_pizza = P.id_pizza
+    orders  orde
+INNER JOIN pizzas pizz ON
+    orde.id_pizza = pizz.id_pizza
 GROUP BY
-    O.id_pizza
+    orde.id_pizza
 ORDER BY
     nbOrders
 ASC
 LIMIT 1;
+
+-- Verification du solde
+SELECT
+    acco.account_balance
+FROM
+    account acco
+WHERE  
+    acco.id_client = 1; -- => $clientId
+
+-- Facturation de la commande
+UPDATE account acco
+SET
+    acco.account_balance = acco.account_balance -(
+        -- Order price
+    SELECT
+        pizz.price * pisi.multiplicator AS price
+    FROM
+        orders AS orde
+    JOIN pizzas AS pizz
+    ON
+        pizz.id_pizza = orde.id_pizza
+    JOIN pizzasizes AS pisi
+    ON
+        pisi.id_size = orde.id_size
+    WHERE
+        orde.id_order = 1 -- => $orderId
+    )
+WHERE
+    acco.id_client = 1; -- => $clientId
