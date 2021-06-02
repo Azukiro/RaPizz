667900c439f0fd414c05d15f2ce4cff1b42b4aa6
Loïc FOURNIER <loic25112013@gmail.com>
2021-06-02 09:27:54 +0200

    Update Loic.sql

--- SQL/requests/Loic.sql
+++ SQL/requests/Loic.sql
@@ -1,1 +1,48 @@
-SELECT C.firstname, C.lastname, C.id_client ,COUNT(DISTINCT O.id_client) as "nbOrders" FROM orders O INNER JOIN clients C ON O.id_client = C.id_client; //Meilleur Client
+---Meilleur Client
+SELECT
+    C.firstname,
+    C.lastname,
+    O.id_client,
+    COUNT(O.id_client) AS nbOrders
+FROM
+    orders O
+INNER JOIN clients C ON
+    O.id_client = C.id_client
+GROUP BY
+    O.id_client
+ORDER BY
+    nbOrders
+DESC
+LIMIT 1;
+
+--Meilleur pizza
+SELECT
+    P.label,
+    O.id_pizza,
+    COUNT(O.id_pizza) AS nbOrders
+FROM
+    orders O
+INNER JOIN pizzas P ON
+    O.id_pizza = P.id_pizza
+GROUP BY
+    O.id_pizza
+ORDER BY
+    nbOrders
+DESC
+LIMIT 1;
+
+--Pire pizza
+SELECT
+    P.label,
+    O.id_pizza,
+    COUNT(O.id_pizza) AS nbOrders
+FROM
+    orders O
+INNER JOIN pizzas P ON
+    O.id_pizza = P.id_pizza
+GROUP BY
+    O.id_pizza
+ORDER BY
+    nbOrders
+ASC
+LIMIT 1;
