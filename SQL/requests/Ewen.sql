e96a2a383036e6d5a1b38b4d9b5f18aa72679d56
iFairPlay22 <ewen.bouquet@gmail.com>
2021-06-02 08:22:02 +0200

    V2

--- SQL/requests/Ewen.sql
+++ SQL/requests/Ewen.sql
@@ -8,76 +8,81 @@
             AND
         ac.password = "c2fd602dba0e90c5418ce9fcbf0af9052e0e14b8";
 
--- 1) Menu
-    SELECT 
-        pizz.id_pizza,
-        pizz.label,
-        (
-            SELECT 
-                GROUP_CONCAT(ingr.label)
-            FROM composing      AS comp
-            JOIN ingredients    AS ingr     ON comp.id_ingredient = ingr.id_ingredient
-            WHERE comp.id_pizza = pizz.id_pizza
-        )
-    FROM pizzas         AS pizz;
+-- Interrogation de la base de données
 
-    -- ou
-    SELECT 
-        pizz.id_pizza,
-        pizz.label,
-        pizz.price
-    FROM pizzas         AS pizz
-    JOIN composing      AS comp     ON comp.id_pizza       = pizz.id_pizza
-    JOIN ingredients    AS ingr     ON comp.id_ingredient  = ingr.id_ingredient;
+    -- 1) Menu
+        SELECT 
+            pizz.id_pizza,
+            pizz.label,
+            (
+                SELECT 
+                    GROUP_CONCAT(ingr.label)
+                FROM composing      AS comp
+                JOIN ingredients    AS ingr     ON comp.id_ingredient = ingr.id_ingredient
+                WHERE comp.id_pizza = pizz.id_pizza
+            )
+        FROM pizzas         AS pizz;
 
--- 3) Questions diverses    
--- Quels sont les véhicules n’ayant jamais servi ?
-    SELECT 
-        DISTINCT(vehi.id_vehicle),
-        vehi.label
-    FROM vehicles           AS vehi     
-    LEFT JOIN orders        AS orde     ON orde.id_vehicle = vehi.id_vehicle
-    WHERE orde.id_vehicle IS NULL
-    GROUP BY vehi.id_vehicle;
+        -- ou
+        SELECT 
+            pizz.id_pizza,
+            pizz.label,
+            pizz.price
+        FROM pizzas         AS pizz
+        JOIN composing      AS comp     ON comp.id_pizza       = pizz.id_pizza
+        JOIN ingredients    AS ingr     ON comp.id_ingredient  = ingr.id_ingredient;
 
--- Calcul du nombre de commandes par client ?
-    SELECT 
-        clien.id_client,
-        clien.firstname,
-        clien.lastname,
-        COUNT(orde.id_order) AS `orders_nb`
-    FROM clients            AS clien     
-    JOIN orders             AS orde     ON orde.id_client = clien.id_client
-    GROUP BY clien.id_client;
+    -- 3) Questions diverses    
+    -- Quels sont les véhicules n’ayant jamais servi ?
+        SELECT 
+            DISTINCT(vehi.id_vehicle),
+            vehi.label
+        FROM vehicles           AS vehi     
+        LEFT JOIN orders        AS orde     ON orde.id_vehicle = vehi.id_vehicle
+        WHERE orde.id_vehicle IS NULL
+        GROUP BY vehi.id_vehicle;
 
--- Calcul de la moyenne des commandes ?
-    SELECT
-        AVG(pizz.price * size.multiplicator)
-    FROM orders             AS orde
-    JOIN pizzas             AS pizz     ON orde.id_pizza = pizz.id_pizza
-    JOIN pizzasizes              AS size     ON orde.id_size  = size.id_size;
+    -- Calcul du nombre de commandes par client ?
+        SELECT 
+            clien.id_client,
+            clien.firstname,
+            clien.lastname,
+            COUNT(orde.id_order) AS `orders_nb`
+        FROM clients            AS clien     
+        JOIN orders             AS orde     ON orde.id_client = clien.id_client
+        GROUP BY clien.id_client;
+
+    -- Calcul de la moyenne des commandes ?
+        SELECT
+            AVG(pizz.price * size.multiplicator)
+        FROM orders             AS orde
+        JOIN pizzas             AS pizz     ON orde.id_pizza = pizz.id_pizza
+        JOIN pizzasizes              AS size     ON orde.id_size  = size.id_size;
 
 
--- Extraction des clients ayant commandé plus que la moyenne ?
-    SELECT
-        r1_clien.id_client,
-        r1_clien.firstname,
-        r1_clien.lastname
-    FROM clients            AS r1_clien
-    WHERE 
-        EXISTS (
-            SELECT r2_orde.id_order
-                FROM orders         AS r2_orde
-            JOIN pizzas             AS r2_pizz     ON r2_orde.id_pizza   = r2_pizz.id_pizza
-            JOIN pizzasizes              AS r2_size     ON r2_orde.id_size    = r2_size.id_size
-            WHERE 
-                r2_orde.id_client = r1_clien.id_client
-                    AND
-                r2_pizz.price * r2_size.multiplicator > (
-                    SELECT
-                        AVG(r3_pizz.price * r3_size.multiplicator)
-                    FROM orders             AS r3_orde
-                    JOIN pizzas             AS r3_pizz     ON r3_orde.id_pizza = r3_pizz.id_pizza
-                    JOIN pizzasizes         AS r3_size     ON r3_orde.id_size  = r3_size.id_size
-                )
-        )
+    -- Extraction des clients ayant commandé plus que la moyenne ?
+        SELECT
+            r1_clien.id_client,
+            r1_clien.firstname,
+            r1_clien.lastname
+        FROM clients            AS r1_clien
+        WHERE 
+            EXISTS (
+                SELECT r2_orde.id_order
+                    FROM orders         AS r2_orde
+                JOIN pizzas             AS r2_pizz     ON r2_orde.id_pizza   = r2_pizz.id_pizza
+                JOIN pizzasizes              AS r2_size     ON r2_orde.id_size    = r2_size.id_size
+                WHERE 
+                    r2_orde.id_client = r1_clien.id_client
+                        AND
+                    r2_pizz.price * r2_size.multiplicator > (
+                        SELECT
+                            AVG(r3_pizz.price * r3_size.multiplicator)
+                        FROM orders             AS r3_orde
+                        JOIN pizzas             AS r3_pizz     ON r3_orde.id_pizza = r3_pizz.id_pizza
+                        JOIN pizzasizes         AS r3_size     ON r3_orde.id_size  = r3_size.id_size
+                    )
+            )
+
+-- Objectifs du système
+    --  Identification du plus mauvais livreur (nombre de retards dans la livraison) et du véhicule utilisé
